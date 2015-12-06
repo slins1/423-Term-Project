9f28aa3c8af40c7772578ca250e30375ae3075b3
Evan Lewis <evanlws@me.com>
2015-12-06 14:21:06 -0500

    Formatting

--- search_Promotion.php
+++ search_Promotion.php
@@ -19,16 +19,10 @@
 <?php
 require('db_connect.inc');
 connect();
-
     $eventCode = $_POST['adEvent'];
-
-//i added a comment
     $adEvent_search_sql = "SELECT Name FROM AdEvent WHERE EventCode = '$eventCode'";
-
     $adEventResult = mysql_query($adEvent_search_sql);
-
     $row = mysql_fetch_assoc($adEventResult);
-
     $eventName = $row['Name'];
 ?>
 	<center>  
@@ -37,43 +31,38 @@ connect();
     <h2>Search for a Promotion to add to the Ad Event <?php echo "$eventName";?>:</h2>
     <h4>At least one field is required</h4>
     <table>
-        <tr>
-            <td><p><b>Promo Code:</b></td>
-            <td><input type="text" name="promoCode" id="promoCode"
-                    placeholder="Enter a Promo Code"></p><span id="errorItemNum" class="error"></span>
-                  <span id="successItemNum" class="correct"></span></td>
-        </tr>
-        <tr>
-            <td><p><b>Name:</b></td>
-            <td><input type="text" name="name" id="name"
-                        placeholder= "Enter a Name"></p><span id="errorItemNum" class="error"></span>
-                  <span id="successItemNum" class="correct"></span></td>
-        </tr>
-        <tr>
-            <td><p><b>Description:</b></td>
-            <td><input type="text" name="description" id="description" placeholder="Enter a Description"></p><span id="errorItemNum" class="error"></span>
-                  <span id="successItemNum" class="correct"></span></td>
-        </tr>
-        <tr>
-            <td><p><b>Amount Off:</b></td>
-            <td><input type="text" name="amountOff" id="amountOff" placeholder="Enter an Amount off"></p><span id="errorItemNum" class="error"></span>
-                  <span id="successItemNum" class="correct"></span></td>
-        </tr>
-        <tr>
-            <td><p><b>Promotion Type:</b></td>
-            <td><div class="dropDown"><select name="promoType" id="promoType">
-                    <option>---</option>
-                    <option>Dollar</option>
-                    <option>Percent</option>
-                    </select></div>
-            </p></td>
-        </tr>
+      <tr>
+        <td><p><b>Promo Code:</b></td>
+        <td><input type="text" name="promoCode" id="promoCode" placeholder="Enter a Promo Code"></p><span id="errorItemNum" class="error"></span>
+        <span id="successItemNum" class="correct"></span></td>
+      </tr>
+      <tr>
+        <td><p><b>Name:</b></td>
+        <td><input type="text" name="name" id="name" placeholder= "Enter a Name"></p><span id="errorItemNum" class="error"></span>
+        <span id="successItemNum" class="correct"></span></td>
+      </tr>
+      <tr>
+        <td><p><b>Description:</b></td>
+        <td><input type="text" name="description" id="description" placeholder="Enter a Description"></p><span id="errorItemNum" class="error"></span>
+        <span id="successItemNum" class="correct"></span></td>
+      </tr>
+      <tr>
+        <td><p><b>Amount Off:</b></td>
+        <td><input type="text" name="amountOff" id="amountOff" placeholder="Enter an Amount off"></p><span id="errorItemNum" class="error"></span>
+        <span id="successItemNum" class="correct"></span></td>
+      </tr>
+      <tr>
+        <td><p><b>Promotion Type:</b></td>
+        <td><div class="dropDown"><select name="promoType" id="promoType">
+	            <option>---</option>
+	            <option>Dollar</option>
+	            <option>Percent</option>
+	            </select></div>
+         </p></td>
+      </tr>
 </table>
         
-  
 <?php
-
-
   echo <<<EOD
     <input type="hidden" name="eventCode" value="$eventCode">
     <input type="hidden" name="eventName" value="$eventName">
@@ -81,10 +70,11 @@ EOD;
 ?>
 
 <p>			
-		<button type="reset" name="reset" accesskey="R" class="button">Reset</button>
-		<button type="submit" name="submit" value="Submit" accesskey="S" class="button">Submit</button>
-</p></form>
+	<button type="reset" name="reset" accesskey="R" class="button">Reset</button>
+	<button type="submit" name="submit" value="Submit" accesskey="S" class="button">Submit</button>
+</p>
+</form>
 	<p><br/><a href="index.html"><button name="menu" class="button">Return to Main Menu</button></a></p>
-	</center>
+</center>
 </body>
 </html>
