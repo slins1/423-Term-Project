a4975df14319dad8ee25a020b9854487fe7a0854
Evan Lewis <evanlws@me.com>
2015-12-06 14:30:03 -0500

    Fixed a bug where the tooltip wasn't fading out

--- _script.js
+++ _script.js
@@ -1,5 +1,15 @@
 $(function() {
-	$(document).tooltip();
+	$(document).tooltip({
+		show: {
+         effect: 'fade'
+        },
+        track: false,
+        open: function (event, ui) {
+            setTimeout(function () {
+                $(ui.tooltip).hide('fade');
+            }, 2000);
+        }
+	});
   $(".button").button();
   $("#startDate, #endDate").datepicker();
 });
