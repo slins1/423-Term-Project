d695b3da2c70faba6bf55903270f0082a3bfd319
evanlws12 <evanlws12@users.noreply.github.com>
2015-12-08 18:53:14 -0500

    Update search_item_for_promotion.php

--- search_item_for_promotion.php
+++ search_item_for_promotion.php
@@ -235,7 +235,7 @@ function isDuplicateItem($itemNumber) {
 ?>
 </table>
 	<p>			
-    	<button class="button" onclick="goBack()">Back</button>
+    		<button class="button" onclick="goBack()">Back</button>
 		<button type="reset" name="reset" accesskey="R" class="button">Reset</button>
 		<button type="submit" name="submit" value="Submit" accesskey="S" class="button">Submit</button>
 </p></form>
